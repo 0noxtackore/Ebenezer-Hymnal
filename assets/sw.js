16b394336ecd4b325561567a9787c7145e6b5395
0noxtackore <tutequeman@gmail.com>
2026-09-01 14:05:11 -0400

    fix: sw skip POST requests to prevent cache error

--- assets/sw.js
+++ assets/sw.js
@@ -1,8 +1,6 @@
 const CACHE_NAME = 'himnario-v1'
-const URLS = ['/', '/index.html']
 
-self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then((c) => c.addAll(URLS)))
+self.addEventListener('install', () => {
   self.skipWaiting()
 })
 
@@ -16,11 +14,14 @@ self.addEventListener('activate', (e) => {
 })
 
 self.addEventListener('fetch', (e) => {
+  if (e.request.method !== 'GET') return
   e.respondWith(
     fetch(e.request)
       .then((res) => {
-        const clone = res.clone()
-        caches.open(CACHE_NAME).then((c) => c.put(e.request, clone))
+        if (res.ok) {
+          const clone = res.clone()
+          caches.open(CACHE_NAME).then((c) => c.put(e.request, clone))
+        }
         return res
       })
       .catch(() => caches.match(e.request))
