b7d4a1106fef910e27bd86059b2c7a8efb8c4de4
0noxtackore <tutequeman@gmail.com>
2026-08-31 20:38:53 -0400

    feat: move Firebase config to .env variables

--- src/firebase.js
+++ src/firebase.js
@@ -5,14 +5,14 @@ import { getDatabase } from 'firebase/database'
 import { getAuth } from 'firebase/auth'
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyCPnsegptU8dCOOimhsPSmHUE5KwbXiDoM',
-  authDomain: 'ebenezer-hymnal.firebaseapp.com',
-  databaseURL: 'https://ebenezer-hymnal-default-rtdb.europe-west1.firebasedatabase.app',
-  projectId: 'ebenezer-hymnal',
-  storageBucket: 'ebenezer-hymnal.firebasestorage.app',
-  messagingSenderId: '1011443273940',
-  appId: '1:1011443273940:web:ca995d7fe8542ec929eb41',
-  measurementId: 'G-5HJTXQLB6C'
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 }
 
 const app = initializeApp(firebaseConfig)
